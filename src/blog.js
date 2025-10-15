3b9016fce1d2a1cd8631fae17bb7827d42425157
ar1nuihc <aron.chiu21@gmail.com>
2025-10-15 00:40:55 -0700

    feat: adjusted spacing

--- src/blog.js
+++ src/blog.js
@@ -29,25 +29,32 @@ function renderBlogs(containerId, blogs) {
         // Create the blog post container <div>
         var post = document.createElement("div");
         post.className = "blog-post";
+        
         // Create child elements
         var titleEl = document.createElement("h1");
         titleEl.className = "blog-title";
         titleEl.textContent = blog.title;
+       
         var imgEl = document.createElement("img");
         imgEl.className = "blog-image";
         imgEl.src = blog.image;
         imgEl.alt = blog.imageAlt;
         imgEl.loading = "lazy";
+       
         var descEl = document.createElement("p");
         descEl.className = "blog-desc";
         descEl.textContent = blog.description;
+        
         // Append children to the post <div>
         post.append(titleEl, imgEl, descEl);
+        
         // Append the post <div> to the main container
+       
         frag.appendChild(post);
     });
     container.appendChild(frag);
 }
+
 // Run after the DOM is ready
 document.addEventListener("DOMContentLoaded", function () {
     renderBlogs("blog-list", Blogs);
